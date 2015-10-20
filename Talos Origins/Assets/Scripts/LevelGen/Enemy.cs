a765b459bdcd14bdb23358c30451e3b35c55540e
Eric Provencher <eprovencher92@gmail.com>
2015-10-20 11:34:03 -0400

    Setting up engine + beginning of enemy scripting

--- Talos Origins/Assets/Scripts/LevelGen/Enemy.cs
+++ Talos Origins/Assets/Scripts/LevelGen/Enemy.cs
@@ -1,38 +1,91 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
-
-    // Maybe change enemy types?
+public class Enemy : MonoBehaviour {   
     public enum eClass { flyer, walker, runner };
     public eClass type;
 
-    int currentLevel;
+    Vector2[] possibleDirections = { Vector2.left, Vector2.right, Vector2.up, Vector2.down };
 
+    Animator animController;
+    Rigidbody2D rb;
+
+    int currentLevel;
     int expGiven;
-        
+
+    LayerMask ignoreLayer, defaultLayer;
+
+    
+
     void Start () {
         //Write Code for Modifying stats based on currentLevel
 
-
-	    //Write logic for setting enemy type
+        //Write logic for setting enemy type
+        type = eClass.flyer;
+        gameObject.tag = "flyer";
 
         //Write logic for changing enemy sprite 
 
-	}	
-	
-	void Update () {
-	
-	}
+        // Initialize variables
+        defaultLayer = LayerMask.NameToLayer("Enemy");
+        ignoreLayer = LayerMask.NameToLayer("Ignore");
+
+        currentLevel = 1;
+        expGiven = 50 * (1 + currentLevel/2);
+    }
+
+    void Update()
+    {
+        // Update according to enemy class
+        switch(type)
+        {
+            case eClass.flyer:
+                {
+                    FlyerUpdate();
+                    break;
+                }
+            case eClass.runner:
+                {
+                    WalkerUpdate();
+                    break;
+                }
+            case eClass.walker:
+                {
+                    RunnerUpdate();
+                    break;
+                }
+        }
+    }
 
 
     void FlyerUpdate()
-    { }
+    {
+        
+    }
 
     void WalkerUpdate()
     { }
 
     void RunnerUpdate()
-    { } 
-    
+    { }
+
+    void SetCollisionWithPlayer(bool enabled)
+    {
+        if (enabled)
+        {
+            gameObject.tag = "Enemy";
+            gameObject.layer = defaultLayer;
+        }
+        else
+        {
+            gameObject.tag = "Ignore";
+            gameObject.layer = ignoreLayer;
+        }
+    }
+
+    // Message Receive function to communicate level and change enemy stats
+    void UpdateLevel()
+    {
+
+    }    
 }
