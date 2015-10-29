ff0fa71c3e60c7ad6fcdd74f58d95d206cd80463
ephilippona <ephilippona@gmail.com>
2015-10-29 11:31:57 -0400

    #16
    Raycasting

--- Talos Origins/Assets/Scripts/Player/Grapple.cs
+++ Talos Origins/Assets/Scripts/Player/Grapple.cs
@@ -4,17 +4,23 @@ using System.Collections;
 public class Grapple : MonoBehaviour {
 
     [SerializeField]
-    GameObject Anchor;
+    GameObject anchor;
+
+    [SerializeField]
+    LineRenderer lineRenderer;
 
     DistanceJoint2D grapple;
     Vector3 targetPosition;
+    Vector3 targetDirection;
+    bool grapplehooked;
 
 	// Use this for initialization
 	void Start () {
         grapple = GetComponent<DistanceJoint2D>();
         grapple.enabled = false;
-        grapple.connectedBody = Anchor.gameObject.GetComponent<Rigidbody2D>();
-
+        grapple.connectedBody = anchor.gameObject.GetComponent<Rigidbody2D>();
+        grapplehooked = false;
+        
     }
 
     // Update is called once per frame
@@ -22,27 +28,54 @@ public class Grapple : MonoBehaviour {
 
        if (Input.GetKeyDown (KeyCode.Mouse1))
        {
-           targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-           targetPosition.z = 0;
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.z = 0;
 
-            Anchor.gameObject.transform.position = targetPosition;
+            targetDirection = targetPosition - transform.position;
+            targetDirection.Normalize();
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, targetDirection);
 
-
-            float distance = Mathf.Abs(Vector3.Distance(targetPosition, transform.position));
-            Debug.Log(distance);
-
-            grapple.distance = distance;
-            grapple.enabled = true;
-                
-            
+            moveHook(hit.point);
+            drawLine();
+            lineRenderer.enabled = true;
+            grapplehooked = true;
 
         }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
             grapple.enabled = false;
+            lineRenderer.enabled = false;
+            lineRenderer.SetPosition(1,transform.position);
 
         }
 
+        if (grapplehooked)
+        {
+            drawLine();
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                grapple.distance -= grapple.distance*0.01f;
+            }
+        }
 	}
+
+    void drawLine()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, anchor.transform.position);
+        lineRenderer.enabled = true;
+    }
+
+    void moveHook(Vector3 anchorPosition)
+    {
+              
+        anchor.gameObject.transform.position = anchorPosition;
+
+        float distance = Mathf.Abs(Vector3.Distance(anchorPosition, transform.position));
+      
+        grapple.distance = distance;
+        grapple.enabled = true;
+    }
 }
