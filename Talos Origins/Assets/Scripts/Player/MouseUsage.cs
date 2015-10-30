df0ec72885474a21b2da45f062f706d0693b7996
ephilippona <ephilippona@gmail.com>
2015-10-29 22:40:26 -0400

    #1 Add aiming reticle
    As well as removing mouse cursor

--- Talos Origins/Assets/Scripts/Player/MouseUsage.cs
+++ Talos Origins/Assets/Scripts/Player/MouseUsage.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class MouseUsage : MonoBehaviour {
 
+	Vector3 mousePosition;
 	// Use this for initialization
 	void Start () {
-	
+		Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.position = new Vector3(mousePosition.x, mousePosition.y, -1f);
 	}
 }
