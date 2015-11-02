bb6dcce1f1f9450840cc444c530ad0b4d3e7fdf3
ephilippona <ephilippona@gmail.com>
2015-11-01 22:22:57 -0500

    #24 Player prefs set up in the shop scene

---
+++ Talos Origins/Assets/UpdatePlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdatePlayer : MonoBehaviour {
+
+	public void UpdateTalosUpgrade(GameObject dropZone){
+		string name = dropZone.transform.GetChild(0).name;
+
+		PlayerPrefs.SetInt (name, (PlayerPrefs.GetInt(name) + 1 ));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
