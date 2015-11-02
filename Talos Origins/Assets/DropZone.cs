0a9e434eeeeacb11dcc758cfc140dee7822589c7
ephilippona <ephilippona@gmail.com>
2015-11-02 00:13:11 -0500

    #24 Fixes to the Shop UI

--- Talos Origins/Assets/DropZone.cs
+++ Talos Origins/Assets/DropZone.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System;
 
 public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
 
@@ -20,15 +21,21 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 	}
 
 	public void OnPointerEnter(PointerEventData eventData){
-		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
-		if (d != null) {
+
+        try { 
+            Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+  
 
 			if(transform.childCount == 0 || gameObject.tag != "Player"){
 				GetComponent<Image> ().color = UnityEngine.Color.green;
 			}else if(transform.childCount >= 1 && gameObject.tag == "Player"){
 				d.GetComponent<Image> ().color = UnityEngine.Color.red;
 			}
-		}
+        }
+        catch (Exception e)
+        {
+            //Do Nothing
+        }
 	}
 	public void OnPointerExit(PointerEventData eventData){
 		GetComponent<Image> ().color = UnityEngine.Color.white;			
