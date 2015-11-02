94e6d66e7f6563893830269e09a5e487f9d9bd59
ephilippona <ephilippona@gmail.com>
2015-11-01 20:17:36 -0500

    #24 Started the shop scene
    - Created the drag and drop mechanic
    - Created cards for the power up

---
+++ Talos Origins/Assets/DropZone.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+  
+	public void OnDrop(PointerEventData eventData)
+	{
+		GetComponent<Image> ().color = UnityEngine.Color.gray;
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
+		d.GetComponent<Image> ().color = UnityEngine.Color.white;
+		if (d != null) {
+			if(transform.childCount < 4 || gameObject.tag != "Player"){
+				d.parentToReturnTo = this.transform;
+			}
+		}
+	}
+
+	public void OnPointerEnter(PointerEventData eventData){
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (d != null) {
+
+			if(transform.childCount < 4 || gameObject.tag != "Player"){
+				GetComponent<Image> ().color = UnityEngine.Color.green;
+			}else if(transform.childCount >= 4 && gameObject.tag == "Player"){
+				d.GetComponent<Image> ().color = UnityEngine.Color.red;
+			}
+		}
+	}
+	public void OnPointerExit(PointerEventData eventData){
+		GetComponent<Image> ().color = UnityEngine.Color.gray;
+	}
+}
---
+++ Talos Origins/Assets/Scripts/Shop/Draggable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler{
+
+	public Transform parentToReturnTo = null;
+
+	public void OnBeginDrag(PointerEventData eventData){	
+		parentToReturnTo = this.transform.parent;
+		this.transform.SetParent (this.transform.parent.parent);
+
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData eventData){
+		gameObject.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData){
+		this.transform.SetParent (parentToReturnTo);
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+	}
+
+}
