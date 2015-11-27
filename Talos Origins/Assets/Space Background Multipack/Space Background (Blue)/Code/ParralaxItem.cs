712021e3255c1c2c944a0ec902c621e0b186de43
Eric Provencher <eprovencher92@gmail.com>
2015-11-26 22:36:45 -0500

    Tweaked background parallax

--- Talos Origins/Assets/Space Background Multipack/Space Background (Blue)/Code/ParralaxItem.cs
+++ Talos Origins/Assets/Space Background Multipack/Space Background (Blue)/Code/ParralaxItem.cs
@@ -10,17 +10,24 @@ class ParralaxItem : MonoBehaviour
     public Vector3 rotationAxis;
     public float rotationSpeed;
     Vector3 direction;
-
+    GameObject Talos;
 
     void Start()
     {
-        direction = new Vector3(Random.Range(minDirection.x, maxDirection.x), Random.Range(minDirection.y, maxDirection.y), Random.Range(minDirection.z, maxDirection.z));
+        Talos = GameObject.Find("Talos");
+        direction = new Vector3(Random.Range(minDirection.x, maxDirection.x), Random.Range(minDirection.y, maxDirection.y), 0);
     }
 
     void Update()
     {
         transform.position += direction * Time.deltaTime;
-
         transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime);
+
+        if(Time.time % 31 == 0)
+        {
+            direction = (Talos.transform.position - transform.position).normalized * 0.1f;
+            direction.z = 0;
+        }
+
     }
 }
