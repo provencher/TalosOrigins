4ff4a905e82809226da7636ceea92a8c1cf5e771
happysheep0719 <lang_happy@hotmail.com>
2015-11-29 00:01:58 -0500

    pause

---
+++ Talos Origins/Assets/Scripts/Interface/PauseControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseControl : MonoBehaviour {
+
+    public bool paused;
+
+	// Use this for initialization
+	void Start () {
+        paused = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
