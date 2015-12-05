ecdd669af8bb37eeac3afa60fa26b75492204061
Eric Provencher <eprovencher92@gmail.com>
2015-12-05 11:11:03 -0500

    Nerfed orb pickups

--- Talos Origins/Assets/Scripts/Player/Player.cs
+++ Talos Origins/Assets/Scripts/Player/Player.cs
@@ -681,7 +681,7 @@ public class Player : MonoBehaviour
     void PickupOrb(int type)
     {
         //Debug.Log("Picked Orb of type " + type.ToString());
-        orbTank[type] += Mathf.CeilToInt(Mathf.Pow(2,(mCurrentLevel-1)));
+        orbTank[type] += Mathf.CeilToInt(Mathf.Pow(1.95f,(mCurrentLevel-1)));
     }
 
     void OnTriggerEnter2D(Collider2D other)
